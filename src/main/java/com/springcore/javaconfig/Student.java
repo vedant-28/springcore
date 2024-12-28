bff008e71b230fb66bc4573594fa2110193c7f91
Vedant <vedant10vaidya@gmail.com>
2024-12-29 02:13:04 +0900

    Spring core | Removing XML config | @component, @Value,  @Configuration in java based config

---
+++ src/main/java/com/springcore/javaconfig/Student.java
@@ -0,0 +1,32 @@
+package com.springcore.javaconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	
+	@Value("1001")
+	private int id;
+	
+	@Value("ABC")
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+}
